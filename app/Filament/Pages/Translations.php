fb93fc8a5fd34545b89fbbb42b57c15fc2157450
nero177 <edwardnero2020@gmail.com>
2025-11-26 22:18:23 +0200

    test fix

--- app/Filament/Pages/Translations.php
+++ app/Filament/Pages/Translations.php
@@ -39,6 +39,7 @@ class Translations extends Page
         foreach ($this->translations as $locale => $content) {
             $decoded = json_decode($content, true);
             if (json_last_error() !== JSON_ERROR_NONE) {
+                dd($content);
                 $this->statusType = 'error';
                 $this->statusMessage = "Invalid JSON for locale {$locale}: " . json_last_error_msg();
                 return;
